5a0bf3c665abb2dbb4e2149e93678fcee9284a44
KP <cle.keyur@gmail.com>
2019-06-11 19:19:16 -0400

    functioning nav

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import Nav from './components/Nav';
+
+import Home from './pages/Home';
+import UserDashboard from './pages/UserDashboard';
+import NotFound from './pages/NotFound';
+
 import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 function App() {
   return (
-    <router>
-      
-    </router>
+    <Router>
+      <div>
+        <Nav />
+        <switch>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/UserDashboard" component={UserDashboard}/>
+          <Route component={NotFound}/>
+        </switch>
+      </div>
+    </Router>
   );
 }
 
