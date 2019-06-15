8a1417646624485dc43739b207f3ef7040f0e8d5
Your Name <tundun.deborah@gmail.com>
2019-06-15 17:11:23 -0400

    "cleaning"

--- server.js
+++ server.js
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-const cors = require("cors");
-const express = require("express");
-const session = require('express-session');
-const db = require("./models");
-const app = express();
-const PORT = process.env.PORT || 3001;
-const routes = require("./routes");
-
-
-// Middleware
-app.use(express.urlencoded({
-  extended: true
-}));
-app.use(express.json());
-
-
-// We need to use sessions to keep track of our user's login status
-app.use(session({
-  secret: "keyboard cat",
-  resave: true,
-  saveUninitialized: true
-}));
-
-
-var syncOptions = {
-  force: false
-};
-
-//serve up static assets
-=======
 const express = require("express");
 var cors = require('cors');
 
@@ -41,27 +10,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Serve up static assets
->>>>>>> 7523d29003b37790a70a1d72be4619b9526bd3db
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
 app.use(cors());
-<<<<<<< HEAD
-app.use(routes);
-// Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function () {
-  app.listen(PORT, function () {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
-  });
-});
-
-module.exports = app;
-=======
 // Add routes, both API and view
 app.use(routes);
 
@@ -69,4 +22,3 @@ app.use(routes);
 app.listen(PORT, () =>
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
 );
->>>>>>> 7523d29003b37790a70a1d72be4619b9526bd3db
