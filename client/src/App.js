883bda51d94fae09aba0acc7c472d42e29f4754f
KP <cle.keyur@gmail.com>
2019-06-15 10:31:14 -0400

    updated navbar with sign in and signup

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import Nav from './components/Navbar';
 import Home from './pages/Home';
 import UserDashboard from './pages/UserDashboard';
 import NotFound from './pages/NotFound';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
@@ -15,6 +17,8 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/UserDashboard" component={UserDashboard} />
+          <Route exact path="/Signin" component={Signin} />
+          <Route exact path="/Signup" component={Signup} />
           <Route component={NotFound} />
         </Switch>
       </div>
--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -8,6 +8,8 @@ function Navbar() {
                 <img src="/img/die.png" alt="20-sided-die" width="50" height="50"></img>
             </Link> | &nbsp; */}
             <Link to="/">Mbnb</Link> | &nbsp;
+            <Link to="/Signin">Sign in</Link> | &nbsp;
+            <Link to="/Signup">Sign up</Link> | &nbsp;
             <Link to="/UserDashboard">User Dashboard</Link>
         </div>
     );
