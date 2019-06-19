939f36025cf278c3e0b4cccf8db1053be111d69b
KP <cle.keyur@gmail.com>
2019-06-18 21:07:26 -0400

    added sign in form

--- client/src/pages/Signin.js
+++ client/src/pages/Signin.js
@@ -1,8 +1,21 @@
 import React from "react";
 
 function Signin() {
-    return(
-        <div>Signin</div>
+    return (
+        <div className="container col-md-6 m-5">
+            <form>
+                <div className="form-group">
+                    <label for="Email"> Email Address</label>
+                    <input type="email" className="form-control" id="email" aria-describedby="emailHelp"  placeholder="Enter email"></input>
+                    <small id="emailHelp" className="form-text text-muted"> We''ll never share your email with anyone else.</small>
+                </div>
+                <div className="form-group">
+                    <label for="Password"> Password </label>
+                    <input type="password" className="form-control" id="Password" placeholder="Password"></input>
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+        </div>
     );
 }
 
