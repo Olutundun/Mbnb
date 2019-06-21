59496e127158d583cd71ea3f70ff64234be4668d
albster <albster@mac.com>
2019-06-20 21:29:34 -0400

    edit

---
+++ client/src/pages/Home.js
@@ -0,0 +1,23 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
+import React from "react";
+import "./Home.css";
+
+function Home() {
+    return(
+        <div className="Main-div">
+            <div className="container">
+                <div className="jumbotron mt-3" id="main-jumbo">
+                <h3 className="text-center" id="headline">Welcome to the new way to get the equipment you need without breaking the bank</h3>
+                
+                
+                
+                </div>
+                    
+            </div>
+        </div>
+    );
+
+    
+}
+
+export default Home;
