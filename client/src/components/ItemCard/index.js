9d067b5ee37c919705579e73c22f438641e5efb3
bryanbuckus <bryanbuckus@gmail.com>
2019-06-20 20:42:04 -0400

    saving changes

--- client/src/components/ItemCard/index.js
+++ client/src/components/ItemCard/index.js
@@ -2,13 +2,7 @@ import React from 'react';
 
 //design the box that's going to show items for rent
 function ItemCard(prop){
-<<<<<<< HEAD
-<div>
-    
-</div>
-=======
 console.log(prop)
->>>>>>> 44911b3da5bb063dec332a51e3cd6f08b4435aed
 }
 
 export default ItemCard;
