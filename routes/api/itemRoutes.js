6c949f3bcba9edd6a57349d960edea0b5b053b17
Your Name <tundun.deborah@gmail.com>
2019-06-22 12:31:11 -0400

    "item route is working"

--- routes/api/itemRoutes.js
+++ routes/api/itemRoutes.js
@@ -2,15 +2,21 @@ const db = require("../../models")
 const router = require("express").Router();
 
 router
-    .route("/")
-    .get((req, res) => {
-        db.Items.create(req.body).then(function(DBItems) {
-            res.json(DBItems);
+    .route("/items")
+     .get((req, res) => {
+         db.Item.findAll({}).then(function(item) {
+             console.log(item)
+             res.json(item);
         })
     });
+    //     .get((req, res) => {
+    //         db.Item.find(({}) => {
+    //             res.json(dbItem);
+    //         })
+    // });
     // .post(itemController.create);
 
-//matches with "/api/items"
+//matches with "/api/item"
 // router
 //     .route("/:id")
 //     .get(itemController.findById)
