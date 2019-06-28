beebbe65428ed379ff9de93bf804aa7692108f28
ehabrasul <Ehab.ohio@gmail.com>
2019-06-27 20:17:56 -0400

    Add files via upload

---
+++ card.js
@@ -0,0 +1,24 @@
+<Card>
+    <CardImage 
+      source={{uri: '/images/guitar.jpeg'}} 
+      title="Guitar for Rent"
+    />
+    <CardTitle
+      subtitle="Number 6"
+    />
+    <CardContent text="Guitar" />
+    <CardAction 
+      separator={true} 
+      inColumn={false}>
+      <CardButton
+        onPress={() => {}}
+        title="Share"
+        color="#FEB557"
+      />
+      <CardButton
+        onPress={() => {}}
+        title="Explore"
+        color="#FEB557"
+      />
+    </CardAction>
+  </Card>
