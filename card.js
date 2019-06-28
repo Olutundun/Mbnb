20c536f5a120124c7c5bd645e8cd072b7de907f6
ehabrasul <Ehab.ohio@gmail.com>
2019-06-27 21:16:16 -0400

    Add files via upload

--- card.js
+++ card.js
@@ -1,10 +1,22 @@
 <Card>
     <CardImage 
-      source={{uri: '/images/guitar.jpeg'}} 
+      source={{src: '/images/guitar.jpeg'}} 
       title="Guitar for Rent"
     />
+{
+    "id": 1,
+    "item_name" :  "Technics Turntables",
+    "item_description" :  "Two top of the line Technics",
+    "day_cost" : 50,
+    "posted" : false,
+    "category" : null,
+    "images" : "https://images-na.ssl-images-amazon.com/images/I/519QWNCBYBL._sx425_.jpg",
+    "postedBy" : "Bryan",
+    "createdAt" : "2019-06-25T20:00:42.000Z",
+    "updatedAt" : "2019-06-25T20:00:42.000Z"
+},
     <CardTitle
-      subtitle="Number 6"
+      subtitle="Electric Guitar"
     />
     <CardContent text="Guitar" />
     <CardAction 
@@ -12,12 +24,7 @@
       inColumn={false}>
       <CardButton
         onPress={() => {}}
-        title="Share"
-        color="#FEB557"
-      />
-      <CardButton
-        onPress={() => {}}
-        title="Explore"
+        title="Rent"
         color="#FEB557"
       />
     </CardAction>
