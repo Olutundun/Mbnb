b8dac9153fbdfca57e6b3b3af3cf4afac4fde43e
Your Name <tundun.deborah@gmail.com>
2019-07-05 20:49:19 -0400

    "added styling and slider to home page"

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,15 +1,9 @@
 import React from "react";
-import "./Home.css";
+import Slide from "../../components/Slider"
 
 function Home() {
     return(
-        <div className="Main-div">
-            <div className="container">
-                <div className="jumbotron mt-3" id="main-jumbo">
-                <h3 className="text-center" id="headline">Welcome to the new way to get the equipment you need without breaking the bank</h3>
-                </div>
-            </div>
-        </div>
+            <Slide />
     );
 }
 
