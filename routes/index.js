aa4cf2fae63cf107f1797f92347139bd069cad9a
KP <cle.keyur@gmail.com>
2019-07-06 18:16:54 -0400

    category routes api

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const path = require("path");
 const router = require("express").Router();
 const itemRoutes = require("./api/itemRoutes");
-const userRoutes = require("./api/userRoutes")
+const userRoutes = require("./api/userRoutes");
+const categoryRoutes = require("./api/categoryRoutes");
 
 //API routes
 router.use("/", itemRoutes);
 router.use("/", userRoutes);
+router.use("/", categoryRoutes);
 
 //if no API routes are hit, send the react app
 router.use(function(req, res) {
