92568a1bab75d6ee7a50a0387249817fd730b0d3
KP <cle.keyur@gmail.com>
2019-07-08 22:32:48 -0400

    footer component added

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+
+
+const Footer = () => (
+  <footer>
+    
+    <section className="footer-contents">
+      <div className="container">
+          <div className="col-md-12 text-center">
+            <div className="footer-nav">
+              <nav>
+                <ul>
+                  <li><a href="/">Home</a></li>
+                </ul>
+              </nav>
+            </div>
+            <div className="footerText text-center">
+                <p>Welcome to Mbnb!</p>
+            </div>
+          </div>
+      </div>
+    </section>
+  </footer>
+);
+
+export default Footer;
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Footer from './components/Footer'
+import "./index.css"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+ReactDOM.render(
+    <div>
+        <App />
+        <Footer/>
+    </div>,
+    document.getElementById('root'));
 
